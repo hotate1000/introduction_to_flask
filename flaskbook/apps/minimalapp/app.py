38afa736d2d1bbc417261d869d3882ba200ec846
hotate1000 <ooo.336.sss@gmail.com>
2023-04-06 23:53:31 +0900

    viewの追加

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,4 +1,5 @@
 from flask import Flask;
+from flask import render_template;
 
 app = Flask(__name__);
 
@@ -8,6 +9,14 @@ def index():
     return "Hello World!";
 
 
-@app.route("/hello/<name>", methods=["GET"], endpoint="hello-endpoint")
+@app.route("/hello/<name>",
+           methods=["GET"],
+           endpoint="hello-endpoint")
 def hello(name):
     return f"Hello {name}";
+
+
+@app.route("/name/<name>")
+def show_name(name):
+    # name=nameはname変数をviewに渡している
+    return render_template("index.html", name=name);
