7a253535eaedef0ef58c3ed066056c7106753d5f
hotate1000 <ooo.336.sss@gmail.com>
2023-04-07 17:16:44 +0900

    動作確認用サンプル画面の作成

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, url_for;
-# from flask import current_app, g, request;
+from flask import Flask, render_template, url_for, redirect;
+from flask import current_app, g, request;
 
 app = Flask(__name__);
 
@@ -39,3 +39,16 @@ with app.test_request_context():
 
 # with app.test_request_context("/users?updated=true"):
 #     print(request.args.get("updated"));
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html");
+
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        # メールの送信処理
+        return redirect(url_for("contact_complete"));
+    return render_template("contact_complete.html");
