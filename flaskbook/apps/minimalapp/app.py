ba026720f043c176d0f29a15c52e94305a63a212
hotate1000 <ooo.336.sss@gmail.com>
2023-04-07 13:20:04 +0900

    url_forの動作確認

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -1,5 +1,5 @@
-from flask import Flask;
-from flask import render_template;
+from flask import Flask, render_template, url_for;
+# from flask import current_app, g, request;
 
 app = Flask(__name__);
 
@@ -20,3 +20,22 @@ def hello(name):
 def show_name(name):
     # name=nameはname変数をviewに渡している
     return render_template("index.html", name=name);
+
+
+with app.test_request_context():
+    print(url_for("index"));
+    print(url_for("hello-endpoint", name="world"));
+    print(url_for("show_name", name="ichiro", page="1"));
+
+
+# ctx = app.app_context();
+# ctx.push();
+
+# print(current_app.name);
+
+# g.connection = "connection";
+# print(g.connection);
+
+
+# with app.test_request_context("/users?updated=true"):
+#     print(request.args.get("updated"));
