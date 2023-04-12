e81c3548c80c5a06e85123d8e97c5fcf1ca74cf2
hotate1000 <ooo.336.sss@gmail.com>
2023-04-12 22:03:10 +0900

    メール送信用（エラー中、別機能を先に実装）

--- flaskbook/apps/minimalapp/app.py
+++ flaskbook/apps/minimalapp/app.py
@@ -20,6 +20,7 @@ toolbar = DebugToolbarExtension(app);
 app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER");
 app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT");
 app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS");
+app.config["MAIL_USE_SSL"] = os.environ.get("MAIL_USE_SSL");
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME");
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD");
 app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER");
