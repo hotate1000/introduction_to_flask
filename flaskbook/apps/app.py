03119df31b6d2e6a1818546da7e85b50ebc8e74b
hotate1000 <ooo.336.sss@gmail.com>
2023-04-18 20:24:58 +0900

    アプリの分割処理テスト

---
+++ flaskbook/apps/app.py
@@ -0,0 +1,8 @@
+from flask import Flask;
+from apps.crud import views as crud_views;
+
+
+def create_app():
+    app = Flask(__name__);
+    app.register_blueprint(crud_views.crud, url_prefix="/crud");
+    return app;
