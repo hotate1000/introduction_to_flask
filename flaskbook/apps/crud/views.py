9fc6657850025d0f5dc4f601edc1bba0a264948b
hotate1000 <ooo.336.sss@gmail.com>
2023-04-20 01:48:06 +0900

    cssのリンク修正

--- flaskbook/apps/crud/views.py
+++ flaskbook/apps/crud/views.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, render_template;
-from apps.app import db;
-from apps.crud.models import User;
 
 
 crud = Blueprint(
@@ -14,9 +12,3 @@ crud = Blueprint(
 @crud.route("/")
 def index():
     return render_template("crud/index.html");
-
-
-@crud.route("/sql")
-def sql():
-    db.session.query(User).all();
-    return;
