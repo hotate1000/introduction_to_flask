8127b881116ba7fe2866a7b17fa21e9e0154916d
hotate1000 <ooo.336.sss@gmail.com>
2023-04-20 22:35:11 +0900

    SQLの結果をコンソールに出力

--- flaskbook/apps/app.py
+++ flaskbook/apps/app.py
@@ -16,39 +16,14 @@ def create_app():
     app.config.from_mapping(
         SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
         SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        # sqlの結果をコンソールに出力
+        SQLALCHEMY_ECHO=True
     )
     db.init_app(app);
     Migrate(app, db);
     app.register_blueprint(crud_views.crud, url_prefix="/crud");
     return app;
-
-# class User(db.Model):
-#     __tablename__ = "users";
-
-#     id = db.Column(db.Integer, primary_key=True);
-#     username = db.Column(db.String, index=True);
-#     email = db.Column(db.String, unique=True, index=True);
-#     password_hash = db.Column(db.String);
-#     created_at = db.Column(db.DateTime, default=datetime.now);
-#     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now);
-
-#     @property
-#     def password(self):
-#         raise AttributeError("読み取り不可");
-
-#     @password.setter
-#     def password(self, password):
-#         self.password_hash = generate_password_hash(password);
-# db = SQLAlchemy();
-
-# app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ";
-# app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}";
-# app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False;
-# # app.config["SQLALCHEMY_ECHO"] = True;
-
-# db.init_app(app);
-# Migrate(app, db);
 
 
 # class User(db.Model):
