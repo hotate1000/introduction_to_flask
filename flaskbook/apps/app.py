5e0f4157e562e888d6c4a63e9f333d40c3863315
hotate1000 <ooo.336.sss@gmail.com>
2023-04-24 23:24:07 +0900

    環境の整理

--- flaskbook/apps/app.py
+++ flaskbook/apps/app.py
@@ -1,25 +1,26 @@
 from flask import Flask;
 from apps.crud import views as crud_views;
-from pathlib import Path;
+# from pathlib import Path;
 from flask_migrate import Migrate;
 from apps.app2 import db;
 from flask_wtf.csrf import CSRFProtect;
+from apps.config import config;
 
 
 csrf = CSRFProtect();
 
 
-def create_app():
+def create_app(config_key):
     app = Flask(__name__);
-    app.config.from_mapping(
-        SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
-        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        # sqlの結果をコンソールに出力
-        SQLALCHEMY_ECHO=True,
-        WTF_CSRF_KEY="AuwzyszU5sugKN7KZs6f",
-    )
-
+    app.config.from_object(config[config_key]);
+    # app.config.from_mapping(
+    #     SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+    #     SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+    #     SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    #     # sqlの結果をコンソールに出力
+    #     SQLALCHEMY_ECHO=True,
+    #     WTF_CSRF_KEY="AuwzyszU5sugKN7KZs6f",
+    # )
     csrf.init_app(app);
     db.init_app(app);
     Migrate(app, db);
