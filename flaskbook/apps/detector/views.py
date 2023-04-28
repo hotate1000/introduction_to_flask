f0c0bd450526f6261a21e8725d83ea21ba86357d
hotate1000 <ooo.336.sss@gmail.com>
2023-04-28 20:29:33 +0900

    タグによる画像検索

--- flaskbook/apps/detector/views.py
+++ flaskbook/apps/detector/views.py
@@ -12,6 +12,7 @@ from flask import (
     redirect,
     url_for,
     flash,
+    request,
 )
 from apps.crud.models import db, User;
 from apps.detector.models import UserImage, UserImageTag;
@@ -208,3 +209,51 @@ def delete_image(image_id):
         db.session.rollback();
 
     return redirect(url_for("detector.index"));
+
+
+@detector.route("/images/search", methods=["GET"])
+def search():
+    user_images = db.session.query(User, UserImage).join(UserImage, User.id == UserImage.user_id);
+    # GETパラメータから検索ワードを取得する。
+    search_text = request.args.get("search");
+    user_image_tag_dict = {};
+    filtered_user_images = [];
+    # タグ情報を検索する。
+    for user_image in user_images:
+        # 検索ワードが空
+        if not search_text:
+            user_image_tags = (
+                db.session.query(UserImageTag)
+                .filter(UserImageTag.user_image_id == user_image.UserImage.id)
+                .all()
+            );
+        else:
+            user_image_tags = (
+                db.session.query(UserImageTag)
+                .filter(UserImageTag.user_image_id == user_image.UserImage.id)
+                .filter(UserImageTag.tag_name.like("%" + search_text + "%"))
+                .all()
+            );
+
+            if not user_image_tags:
+                continue
+
+            user_image_tags = (
+                db.session.query(UserImageTag)
+                .filter(UserImageTag.user_image_id == user_image.UserImage.id)
+                .all()
+            );
+
+        user_image_tag_dict[user_image.UserImage.id] = user_image_tags;
+        filtered_user_images.append(user_image);
+
+    delete_form = DeleteForm();
+    detector_form = DetectorForm();
+
+    return render_template(
+        "detector/index.html",
+        user_images=filtered_user_images,
+        user_image_tag_dict=user_image_tag_dict,
+        delete_form=delete_form,
+        detector_form=detector_form
+    );
