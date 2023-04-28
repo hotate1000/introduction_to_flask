057dc72131392ad3176e5497545e41d86b8a164f
hotate1000 <ooo.336.sss@gmail.com>
2023-04-28 22:24:27 +0900

    自作のエラー作成

--- flaskbook/apps/app.py
+++ flaskbook/apps/app.py
@@ -1,4 +1,4 @@
-from flask import Flask;
+from flask import Flask, render_template;
 from apps.crud import views as crud_views;
 from apps.auth import views as auth_views;
 from apps.detector import views as detector_views;
@@ -21,4 +21,14 @@ def create_app(config_key):
     app.register_blueprint(crud_views.crud, url_prefix="/crud");
     app.register_blueprint(auth_views.auth, url_prefix="/auth");
     app.register_blueprint(detector_views.detector, url_prefix="/");
+    app.register_error_handler(404, page_not_found);
+    app.register_error_handler(500, internal_server_error);
     return app;
+
+
+def page_not_found(e):
+    return render_template("404.html"), 404;
+
+
+def internal_server_error(e):
+    return render_template("500.html"), 500;
